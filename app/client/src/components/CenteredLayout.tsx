8f7a0352b160df17fb7babbf25cc799303b7b8ad
Mahesh Paliath - m0p0qkq <Mahesh.Paliath@walmart.com>
2025-06-30 19:18:01 -0700

    Default avatar

--- app/client/src/components/CenteredLayout.tsx
+++ app/client/src/components/CenteredLayout.tsx
@@ -10,6 +10,19 @@ function UserAvatar() {
     return null;
   }
 
+  if (user.id === 'guest') {
+    return (
+      <Avatar className="h-8 w-8">
+        <AvatarFallback title="Guest User">
+          {/* Heroicon: user-circle (solid) */}
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0012 11z" clipRule="evenodd" />
+          </svg>
+        </AvatarFallback>
+      </Avatar>
+    );
+  }
+
   const initials = user.name
     .split(' ')
     .map((n) => n[0])
